98b0d83ef87010525d7db5f131a646bced9b7240
XiaoyuYuan19 <xiaoyuyuan19@gmail.com>
2025-06-29 20:11:56 +0300

    feat: add background music with user interaction trigger

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ export default function App() {
   return (
 
     <div className="h-screen bg-cover bg-center flex flex-col items-center justify-center px-4 relative "  style={{
-  backgroundImage: `url('/assets/cards/bg (3).jpg')`}}>
+  backgroundImage: `url('/assets/cards/bg (4).jpg')`}}>
 
       <div className="absolute inset-0 bg-black/40 backdrop-blur-sm z-0"></div>
       <audio ref={audioRef} src="/assets/audio/bg-music.mp3" autoPlay loop />
